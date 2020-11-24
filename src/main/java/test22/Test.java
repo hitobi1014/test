1332c0c69192085e91951481bd5c704f952afe82
dspark <fmfhfmfh@gmail.com>
2020-11-24 10:20:59 +0900

    커밋입니다

---
+++ src/main/java/test22/Test.java
@@ -0,0 +1,10 @@
+package test22;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("test중");
+		int test = 50;
+		int count = test - 47;
+		System.out.println(count);
+	}
+}
